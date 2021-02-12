80d3ca1733ca03f5fa038baaade2b507b79fe84f
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-02-11 21:36:05 -0600

    Update script.js

--- automate-sms/script.js
+++ automate-sms/script.js
@@ -1,9 +1,12 @@
-var twilio = require('twilio');
-// Linking my Twilio account SID and auth token
-var client = new twilio('AC05d56983046b42e13620a6518a7c3cf8', '26fae5652cb2af0d60569dda733e6bcb');
-// Send the text message
-client.messages.create({
-    to: '+12196718621',
-    from: '+12019879761',
-    body: 'Hi there, Sherouk! I\'m not a creep.'
-});
+function automateSMS () {
+    const twilio = require('twilio')
+    // Linking my Twilio account SID and auth token
+    const client = new twilio('AC05d56983046b42e13620a6518a7c3cf8', '26fae5652cb2af0d60569dda733e6bcb')
+    // Send the text message
+    client.messages.create({
+        to: '+12196718621',
+        from: '+12019879761',
+        body: 'Hi there, Sherouk! How are you?'
+    })
+};
+automateSMS()
