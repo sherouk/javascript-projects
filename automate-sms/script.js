5deeaff0a0ec7f264a7e86e6a57a4bd76d70c2ef
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-02-11 21:52:11 -0600

    Update script.js

--- automate-sms/script.js
+++ automate-sms/script.js
@@ -4,9 +4,9 @@ function automateSMS () {
     const client = new twilio('AC05d56983046b42e13620a6518a7c3cf8', '26fae5652cb2af0d60569dda733e6bcb')
     // Send the text message
     client.messages.create({
-        to: '+12196718621',
+        to: '+12197073645',
         from: '+12019879761',
-        body: 'Hi there, Sherouk! How are you?'
+        body: 'Hi Darth Noona! It\'s Shosho :D'
     })
 };
 automateSMS()
